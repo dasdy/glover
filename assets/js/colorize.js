1ce953c5212f883e28262a82a3ac6336c6135cb6
Oleksandr Podze <dasdeg@gmail.com>
2024-10-22 00:17:57 +0200

    more gradients in js

--- assets/js/colorize.js
+++ assets/js/colorize.js
@@ -1,26 +1,93 @@
-function interpolateColor(value) {
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16),
+      }
+    : null;
+}
+
+let viridis = [
+  hexToRgb("#fde725"),
+  hexToRgb("#7ad151"),
+  hexToRgb("#22a884"),
+  hexToRgb("#2a788e"),
+  hexToRgb("#414487"),
+  hexToRgb("#440154"),
+];
+let inferno = [
+  hexToRgb("#fcffa4"),
+  hexToRgb("#fca50a"),
+  hexToRgb("#dd513a"),
+  hexToRgb("#932667"),
+  hexToRgb("#420a68"),
+  hexToRgb("#000004"),
+];
+let magma = [
+  hexToRgb("#fcfdbf"),
+  hexToRgb("#fe9f6d"),
+  hexToRgb("#de4968"),
+  hexToRgb("#8c2981"),
+  hexToRgb("#3b0f70"),
+  hexToRgb("#000004"),
+];
+
+let default = [hexToRgb("#0000ff"), hexToRgb("#00ff00"), hexToRgb("#ff0000")];
+
+function normalize(n, min, max) {
+  if (min === max) {
+    return 0;
+  }
+  return (n - min) / (max - min);
+}
+function expand(n, min, max) {
+  return (max - min) * n + min;
+}
+function translate(n, min1, max1, min2, max2) {
+  return expand(normalize(n, min1, max1), min2, max2);
+}
+
+function interpolateColor(value, schema) {
+  if (schema === undefined) {
+    schema = default;
+    // schema = viridis.slice().reverse();
+    // schema = magma.slice().reverse();
+    // schema = inferno.slice().reverse();
+  }
+
   let r, g, b;
-
-  if (value > 1) {
-    value = 1;
-  }
-
-  if (value <= 0.5) {
-    // Interpolate between blue (0, 0, 255) and green (0, 255, 0)
-    const ratio = value / 0.5;
-    r = 0;
-    g = Math.round(255 * ratio);
-    b = Math.round(255 * (1 - ratio));
+  if (value >= 1) {
+    let c = schema[schema.length - 1];
+    r = c.r;
+    g = c.g;
+    b = c.b;
+  } else if (value <= 0) {
+    let c = schema[0];
+    r = c.r;
+    g = c.g;
+    b = c.b;
   } else {
-    // Interpolate between green (0, 255, 0) and red (255, 0, 0)
-    const ratio = (value - 0.5) / 0.5;
-    r = Math.round(255 * ratio);
-    g = Math.round(255 * (1 - ratio));
-    b = 0;
+    const N = schema.length - 1, // number of buckets
+      s = 1 / N, // size of each bucket
+      n = Math.floor(value / s), // bucket number
+      minVal = s * n, // bucket min value
+      maxVal = s * (n + 1), // bucket max value
+      A = schema[n], // bucket min color
+      B = schema[n + 1]; //bucket max color
+
+    if (B === undefined) {
+      console.log("heyo");
+    }
+
+    r = translate(value, minVal, maxVal, A.r, B.r);
+    g = translate(value, minVal, maxVal, A.g, B.g);
+    b = translate(value, minVal, maxVal, A.b, B.b);
   }
 
-  let blendFactor = 0.5;
-  // Blend with white (255, 255, 255) to reduce intensity
+  let blendFactor = 0.3;
+  // // Blend with white (255, 255, 255) to reduce intensity
   r = Math.round(r + (255 - r) * blendFactor);
   g = Math.round(g + (255 - g) * blendFactor);
   b = Math.round(b + (255 - b) * blendFactor);
